33df330fbe252cf91efe48f76a550c0ccf3da07e
shreelvi <shreelvi@isu.edu>
2019-05-13 11:08:05 -0600

    scaffolded identity to use in music store project

---
+++ Portfolio_Elvis/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Portfolio_Elvis.Model;
+
+[assembly: HostingStartup(typeof(Portfolio_Elvis.Areas.Identity.IdentityHostingStartup))]
+namespace Portfolio_Elvis.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<IdentityContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("IdentityContextConnection")));
+
+                services.AddDefaultIdentity<IdentityUser>()
+                    .AddEntityFrameworkStores<IdentityContext>();
+            });
+        }
+    }
+}
