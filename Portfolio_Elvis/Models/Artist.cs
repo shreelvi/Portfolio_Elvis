a300ca86e44882ad0aeb626c5a851114f3d83eac
shreelvi <shreelvi@isu.edu>
2019-05-12 23:18:06 -0600

    Added dbcontext class for music store and sample data to seed ef db

--- Portfolio_Elvis/Models/Artist.cs
+++ Portfolio_Elvis/Models/Artist.cs
@@ -5,7 +5,14 @@ using System.Threading.Tasks;
 
 namespace Portfolio_Elvis.Models
 {
+    /// <summary>
+    /// Created by: Elvis
+    /// Date Created: 05/12/2019
+    /// Artist is associated to the album
+    /// </summary>
     public class Artist
     {
+        public int ArtistId { get; set; }
+        public string Name { get; set; }
     }
 }
---
+++ Portfolio_Elvis/Models/MusicStoreContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Elvis.Models
+{
+    /// <summary>
+    /// Created by: Elvis
+    /// This class will represent the Entity Framework database context, 
+    /// and will handle our create, read, update, and delete operations for us.
+    /// https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/mvc-music-store/mvc-music-store-part-4
+    /// </summary>
+    public class MusicStoreContext: DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
