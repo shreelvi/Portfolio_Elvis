708e01a840b304c1b09796fcc7823f5115617d0e
shreelvi <shreelvi@isu.edu>
2019-05-14 16:47:10 -0600

    some changes to finalize identity in musicstore proj

---
+++ Portfolio_Elvis/Areas/Identity/Data/WebApp1User.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Portfolio_Elvis.Areas.Identity.Data
+{
+    public class WebApp1User:IdentityUser
+    {
+        [PersonalData]
+        public string Name { get; set; }
+        [PersonalData]
+        public DateTime DOB { get; set; }
+    }
+}
--- Portfolio_Elvis/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Portfolio_Elvis/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -67,7 +67,7 @@ namespace Portfolio_Elvis.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl = returnUrl ?? Url.Content("~/");
+            returnUrl = returnUrl ?? Url.Content("~/MusicStore/");
 
             if (ModelState.IsValid)
             {
