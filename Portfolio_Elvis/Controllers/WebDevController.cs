1e0a65bffd1a1de2cf8b5e8e4b03b76ae348fdb4
shreelvi <shreelvi@isu.edu>
2019-05-15 17:00:29 -0600

    added link to web dev projects.

---
+++ Portfolio_Elvis/Controllers/WebDevController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Portfolio_Elvis.Controllers
+{
+    // Controls web development section in home page
+    // Web development section includes projects web app projects that I have worked in past
+    public class WebDevController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
