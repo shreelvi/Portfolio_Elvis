626fc26a1f7db1f58c3ce75505393aab65ec6a91
shreelvi <shreelvi@isu.edu>
2019-05-16 13:11:13 -0600

    home page update to show top-selling albums

--- Portfolio_Elvis/Controllers/MusicStore/MusicStoreHomeController.cs
+++ Portfolio_Elvis/Controllers/MusicStore/MusicStoreHomeController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Portfolio_Elvis.Models;
 
 namespace Portfolio_Elvis.Controllers
 {
+    /// <summary>
+    /// Home controller for music store project
+    /// Reference: https://web.csulb.edu/~pnguyen/cecs475/labs/mvc-music-store-tutorial-v30.pdf
+    /// Followed tutorial to develop the project
+    /// </summary>
     public class MusicStoreHomeController : Controller
     {
+        private readonly MusicStoreContext _context;
+
+        public MusicStoreHomeController(MusicStoreContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            // Get most popular albums
+            var albums = GetTopSellingAlbums(5);
+            return View(albums);        }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+            return _context.Albums
+            .OrderByDescending(a => a.OrderDetails.Count())
+            .Take(count)
+            .ToList();
         }
     }
 }
